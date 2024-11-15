6a1577c8d8cad14102ff466a3999410e8eee1b45
Akmitliviy <akmitliviy@gmail.com>
2024-11-16 01:53:24 +0200

    Creating basic classes

---
+++ SecurityManager/src/main/java/manager/SecurityManager.java
@@ -0,0 +1,4 @@
+﻿package manager;
+
+public class SecurityManager {
+}
---
+++ SecurityManager/src/main/java/manager/security/sensors/Sensor.java
@@ -0,0 +1,50 @@
+﻿package manager.security.sensors;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class Sensor implements Flow.Publisher<Boolean> {
+    protected final int id;
+    protected boolean isTriggered;
+    protected final AtomicBoolean status = new AtomicBoolean(false);
+    protected int floorId;
+    protected int roomId;
+    protected Flow.Subscriber<? super Boolean> subscriber;
+
+    public Sensor(int id){
+        this.id = id;
+    }
+
+    public void switchActivation(){}
+    public AtomicBoolean getStatus(){
+        return status;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void trigger(){
+        status.set(true);
+    }
+    public void reset(){
+        status.set(false);
+    }
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super Boolean> subscriber) {
+        this.subscriber = subscriber;
+        subscriber.onSubscribe(new Flow.Subscription() {
+            @Override
+            public void request(long n) {
+
+            }
+
+            @Override
+            public void cancel() {
+
+            }
+        });
+    }
+
+
+}
