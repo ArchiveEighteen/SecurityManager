e37ec21c7989da47495ba6b80089be4d3649da73
Akmitliviy <akmitliviy@gmail.com>
2024-11-16 05:16:43 +0200

    feat: contains basic logic for Floor, Room and SecurityManager

--- SecurityManager/src/main/java/manager/Floor.java
+++ SecurityManager/src/main/java/manager/Floor.java
@@ -1,4 +1,27 @@
 ﻿package manager;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Floor {
+    private UUID id = UUID.randomUUID();
+    private List<Room> rooms = new ArrayList<>();
+
+    public Room addRoom(int windowsAmount, int doorsAmount, double area){
+        var room = new Room(id, windowsAmount, doorsAmount, area);
+        rooms.add(room);
+        return room;
+    }
+    public boolean deleteRoom(UUID roomId){
+        return rooms.removeIf(r -> r.getId().equals(id));
+    }
+
+    public UUID getId(){
+        return id;
+    }
+    public List<Room> getRooms(){
+        return rooms;
+    }
+
 }
--- SecurityManager/src/main/java/manager/Room.java
+++ SecurityManager/src/main/java/manager/Room.java
@@ -1,4 +1,73 @@
 ﻿package manager;
 
+import manager.security.sensorcreators.MotionSensorCreator;
+import manager.security.sensorcreators.OpenSensorCreator;
+import manager.security.sensorcreators.SensorCreator;
+import manager.security.sensorcreators.TemperatureSensorCreator;
+import manager.security.sensors.Sensor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Room {
+    private final UUID id = UUID.randomUUID();
+    private final UUID floorId;
+    private final int windowsAmount;
+    private final  int doorsAmount;
+    private final double area;
+    private final List<Sensor> sensors = new ArrayList<>();
+
+    public Room(UUID floorId, int windowsAmount, int doorsAmount, double area) {
+        this.floorId = floorId;
+        this.windowsAmount = windowsAmount;
+        this.doorsAmount = doorsAmount;
+        this.area = area;
+
+        List<SensorCreator> sensorCreators = GetSensorCreators(windowsAmount, doorsAmount, area);
+
+        for(var sensorCreator : sensorCreators) {
+            sensors.add(sensorCreator.CreateSensor(floorId, id));
+        }
+    }
+
+    private List<SensorCreator> GetSensorCreators(int windowsAmount, int doorsAmount, double area) {
+        List<SensorCreator> sensorCreators = new ArrayList<>();
+
+        // TODO: Read from file that contains standards for amount of sensors per room, create sensor creators
+        // TODO: and add them to sensorCreators list
+
+        // TODO: Delete section below after completing aforementioned task
+        // start section
+        sensorCreators.add(new TemperatureSensorCreator());
+        for(int i = 0; i < doorsAmount+windowsAmount; i++) {
+            sensorCreators.add(new OpenSensorCreator());
+        }
+        for(int i = 0; i < area / 15 + 1; i++) {
+            sensorCreators.add(new MotionSensorCreator());
+        }
+        // end section
+
+        return sensorCreators;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    public UUID getFloorId() {
+        return floorId;
+    }
+    public int getWindowsAmount() {
+        return windowsAmount;
+    }
+    public int getDoorsAmount() {
+        return doorsAmount;
+    }
+    public double getArea() {
+        return area;
+    }
+    public List<Sensor> getSensors() {
+        return sensors;
+    }
+
 }
