ca1f834b49f2428a46a0c92b6616e47a675046f4
Akmitliviy <akmitliviy@gmail.com>
2024-11-16 05:16:43 +0200

    feat: contains basic logic for Floor, Room and SecurityManager

--- SecurityManager/src/main/java/manager/SecurityManager.java
+++ SecurityManager/src/main/java/manager/SecurityManager.java
@@ -1,4 +1,32 @@
 ﻿package manager;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class SecurityManager {
+    private List<Floor> floors = new ArrayList<>();
+    // TODO: Add Journal private field
+
+    public Floor addFloor() {
+        var floor = new Floor();
+        floors.add(floor);
+        return floor;
+    }
+
+    public List<Floor> getFloors() {
+        return floors;
+    }
+
+    public boolean deleteFloor(UUID floorId) {
+        return floors.removeIf(f -> f.getId().equals(floorId));
+    }
+    public boolean deleteRoom(UUID floorId, UUID roomId) {
+        for(Floor floor : floors) {
+            if(floor.getId().equals(floorId)) {
+                return floor.deleteRoom(roomId);
+            }
+        }
+        return false;
+    }
 }
